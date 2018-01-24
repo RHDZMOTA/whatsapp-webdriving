2b8e35f21fb9778983b8461d62809c67eb34c018
rhdzmota <rohdzmota@gmail.com>
2018-01-24 17:27:53 -0600

    add logger

--- main.py
+++ main.py
@@ -1,27 +1,34 @@
 #!/usr/bin/env python3
+import logging
 import time
 
 from selenium import webdriver
 
 from services.whatsapp import WhatsApp
+from settings import LogConf
 from util import Messages, send_message
+from util.operative_system import select_driver
 
-CHROME_DRIVER = "resources/drivers/linux/chromedriver"
 SLEEP_TIME = 3
 
 
-def main():
-    driver = webdriver.Chrome(CHROME_DRIVER, chrome_options=webdriver.ChromeOptions())
+def main(logger, ignore=[]):
+    driver = webdriver.Chrome(select_driver(), chrome_options=webdriver.ChromeOptions())
     whatsapp_service = WhatsApp(driver)
     message_service = Messages()
     for user in message_service.get_users():
+        if user in ignore:
+            logger.info("User %s ignored." % user)
+            continue
         whatsapp_service.open_chat(user)
         time.sleep(SLEEP_TIME)
         messages = message_service.get_messages(user)
         for message in messages:
             send_message(whatsapp_service, message)
+            logger.info("Message send to user: %s." % user)
     whatsapp_service.close()
 
 
 if __name__ == "__main__":
-    main()
+    logger = LogConf.create(logging)
+    main(logger)
