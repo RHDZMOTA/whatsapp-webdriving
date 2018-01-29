f247cea21d21a196504ea4de5325b63907379f8c
rhdzmota <rohdzmota@gmail.com>
2018-01-29 15:16:09 -0600

    implement abstract message class

--- services/messages/messages_file.py
+++ services/messages/messages_file.py
@@ -1,38 +1,16 @@
 import json
-import time
 
-from settings import FileConf, SLEEP_TIME
-from util.messages import send_message
+from services.messages import MessagesAbstract
+from settings import FileConf
 
 
-class MessagesFile(object):
+class MessagesFile(MessagesAbstract):
 
-    def __init__(self, logger):
+    def load_messages(self):
         self.messages = json.loads(open(FileConf.FileNames.messages, "r").read())
-        self.logger = logger
 
-    def get_users(self):
+    def _get_ignored_users(self) -> list:
+        return []
+
+    def _get_users(self) -> list:
         return list(self.messages.keys())
-
-    def get_messages(self, user):
-        return self.messages.get(user)
-
-    @staticmethod
-    def get_ignored_users():
-        return []  # TODO: read file.
-
-    def send_messages(self, whatsapp_service):
-        ignored_users = self.get_ignored_users()
-
-        for user in self.get_users():
-
-            if user in ignored_users:
-                self.logger.info("User %s ignored." % user)
-                continue
-
-            whatsapp_service.open_chat(user)
-            time.sleep(SLEEP_TIME)
-            messages = self.get_messages(user)
-
-            for message in messages:
-                send_message(whatsapp_service, message)
